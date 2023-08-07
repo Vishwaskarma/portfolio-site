7be6efe8d93b1ecb9cbb3082d320f7980f7dce3b
Himanshu vishwakarma <119053035+Vishwaskarma@users.noreply.github.com>
2023-08-06 22:55:41 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.getElementsByTagName('ul');
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+
+  ul.classList.toggle("active");
+});
